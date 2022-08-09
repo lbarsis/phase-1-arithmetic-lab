f7d7984a8eaef2503cd9724576f2fe5e64775916
Levi Barsis <lbarsis@gmail.com>
2022-08-09 10:52:54 -0700

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+let num3 = 10
+let num4 = 6
 
-// Write your code here
+let multiply = num1 * num2
+let random = Math.floor(Math.random()*10) + 1
+let mod = num3 % num4
+let max = Math.max(0,1,3,4,5,20)
+
+console.log(mod)
